ba39ce3b4c76281c96b5971f451d82350382715d
Your Name <you@example.com>
2024-01-12 15:39:55 +0400

    12-01-2024 v1.2

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -3,15 +3,13 @@ package ru.kata.spring.boot_security.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
 import ru.kata.spring.boot_security.demo.entities.User;
 import ru.kata.spring.boot_security.demo.service.UsersServiceImp;
 
-import java.security.Principal;
-
-@RestController
+@Controller
 @RequestMapping("/admin")
 public class AdminController {
 
@@ -22,20 +20,67 @@ public class AdminController {
         this.usersServiceImp = usersServiceImp;
     }
 
+
     @GetMapping("/")
-    public String pageForAdmin(Principal principal) {
-        return "aaaaaaaaaaaaaaaaaaaaa"+" "+principal.getName();
+    public String show(@RequestParam(value = "id", required = false) long id, Model model) {
+        model.addAttribute("getUserId", usersServiceImp.findUserById(id));
+        return "admin/show";
+    }
+    @GetMapping()
+    public String showAll(ModelMap model) {
+        model.addAttribute("userss", usersServiceImp.getAllUsers());
+        return "admin/showAll";
     }
 
-    @GetMapping("/read_profile")
-    public String pageReadProfile(Principal principal) {
-        return "Profile is " + principal.getName();
+    @GetMapping("/new")
+    public String newUser(@ModelAttribute("user") User user) {
+        return "admin/new";
     }
 
-    @GetMapping("/authenticated")
-    public String pageForAuthenticatedUsers(Principal principal) {
-        User user = usersServiceImp.ffindByUserName(principal.getName());
-        return "Secured page for web service "+ user.getUsername()+ " " +user.getPassword();
-//        return "admin/authenticated";
+    @PostMapping()
+    public String create(@ModelAttribute("user") User user, Model model) {
+        usersServiceImp.saveUser(user);
+        return "redirect:/admin";
     }
+
+    @GetMapping("/{id}/edit")
+    public String edit(Model model, @PathVariable("id") long id) {
+        model.addAttribute("user", usersServiceImp.findUserById(id));
+        return "admin/edit";
+    }
+
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("user") User user, @PathVariable("id") long id) {
+        usersServiceImp.updateUser(user);
+        return "redirect:/admin";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") long id) {
+        usersServiceImp.deleteUser(usersServiceImp.findUserById(id));
+        return "redirect:/admin";
+    }
+
+
+
+
+
+
+
+//    @GetMapping("/")
+//    public String pageForAdmin(Principal principal) {
+//        return "aaaaaaaaaaaaaaaaaaaaa"+" "+principal.getName();
+//    }
+//
+//    @GetMapping("/read_profile")
+//    public String pageReadProfile(Principal principal) {
+//        return "Profile is " + principal.getName();
+//    }
+//
+//    @GetMapping("/authenticated")
+//    public String pageForAuthenticatedUsers(Principal principal) {
+//        User user = usersServiceImp.ffindByUserName(principal.getName());
+//        return "Secured page for web service "+ user.getUsername()+ " " +user.getPassword();
+////        return "admin/authenticated";
+//    }
 }
--- src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
@@ -25,46 +25,51 @@ public class UserController {
         this.roleRepository = roleRepository;
     }
 
-
-    @GetMapping()
-    public String showAll(ModelMap model) {
-        model.addAttribute("userss", userService.getAllUsers());
-        return "users/showAll";
-    }
-
     @GetMapping("/")
-    public String show(@RequestParam(value = "id", required = false) int id, Model model) {
+    public String show(@RequestParam(value = "id", required = false) long id, Model model) {
         model.addAttribute("getUserId", userService.findUserById(id));
         return "users/show";
     }
 
-    @GetMapping("/new")
-    public String newUser(@ModelAttribute("user") User user) {
-        return "users/new";
-    }
 
-    @PostMapping()
-    public String create(@ModelAttribute("user") User user, Model model) {
-        userService.saveUser(user);
-        return "redirect:/users";
-    }
 
-    @GetMapping("/{id}/edit")
-    public String edit(Model model, @PathVariable("id") int id) {
-        model.addAttribute("user", userService.findUserById(id));
-        return "users/edit";
-    }
 
-    @PatchMapping("/{id}")
-    public String update(@ModelAttribute("user") User user, @PathVariable("id") int id) {
-        userService.updateUser(user);
-        return "redirect:/users";
-    }
 
-    @DeleteMapping("/{id}")
-    public String delete(@PathVariable("id") int id) {
-        userService.deleteUser(userService.findUserById(id));
-        return "redirect:/users";
-    }
+
+//
+//    @GetMapping()
+//    public String showAll(ModelMap model) {
+//        model.addAttribute("userss", userService.getAllUsers());
+//        return "users/showAll";
+//    }
+//
+//    @GetMapping("/new")
+//    public String newUser(@ModelAttribute("user") User user) {
+//        return "users/new";
+//    }
+//
+//    @PostMapping()
+//    public String create(@ModelAttribute("user") User user, Model model) {
+//        userService.saveUser(user);
+//        return "redirect:/users";
+//    }
+//
+//    @GetMapping("/{id}/edit")
+//    public String edit(Model model, @PathVariable("id") int id) {
+//        model.addAttribute("user", userService.findUserById(id));
+//        return "users/edit";
+//    }
+//
+//    @PatchMapping("/{id}")
+//    public String update(@ModelAttribute("user") User user, @PathVariable("id") int id) {
+//        userService.updateUser(user);
+//        return "redirect:/users";
+//    }
+//
+//    @DeleteMapping("/{id}")
+//    public String delete(@PathVariable("id") int id) {
+//        userService.deleteUser(userService.findUserById(id));
+//        return "redirect:/users";
+//    }
 
 }
