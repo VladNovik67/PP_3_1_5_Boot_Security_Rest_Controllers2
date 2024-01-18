44bc13a24aaa82b40f310cf92467d956cc758db3
Your Name <you@example.com>
2024-01-18 17:15:32 +0400

    18-01-2023 v1.1

--- src/main/java/ru/kata/spring/boot_security/demo/entities/User.java
+++ src/main/java/ru/kata/spring/boot_security/demo/entities/User.java
@@ -21,7 +21,6 @@ public class User implements UserDetails {
     private String email;
 
 
-    //   (cascade = CascadeType.PERSIST)
     @ManyToMany
     @JoinTable(name = "users_roles",
             joinColumns = @JoinColumn(name = "users_id"),
